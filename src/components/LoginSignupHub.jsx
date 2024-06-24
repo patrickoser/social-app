b2dfbbed259b12b0873caf09de410e2aab5b3d32
Patrick Oser <patrick.oser148@gmail.com>
2024-06-24 11:49:53 -0400

    Add content to LoginSignupHub.jsx.

---
+++ src/components/LoginSignupHub.jsx
@@ -0,0 +1,13 @@
+import Signup from './Signup.jsx'
+
+const LoginSignupHub = () => {
+    // Add a feature that checks local storage to see if they
+    // already have an account before redirecting to Signup.
+    return (
+        <main>
+            <Signup />
+        </main>
+    )
+}
+
+export default LoginSignupHub
