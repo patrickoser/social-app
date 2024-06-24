0f0811d5b7f287e2f3967655d88300022b172dc1
Patrick Oser <patrick.oser148@gmail.com>
2024-06-24 11:48:18 -0400

    Add content to Login.jsx.

---
+++ src/components/Login.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account.</h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form onSubmit={handleLogin} className="space-y-6">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
+                        <div className="mt-2">
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Enter email..."
+                                value={email}
+                                autoComplete="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                            <div className="text-sm">
+                                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Enter password..."
+                                value={password}
+                                autoComplete="current-password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign In</button>
+                    </div>
+                </form>
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Don't have an account?&nbsp;
+                    <Link to="/signup"><button className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Signup here!</button></Link>
+                </p>
+            </div>
+        </main>
+    )
+}
+
+export default Login
