0afd16ac32eaa9e20aacb4cef837e6a77df1cf0a
Patrick Oser <patrick.oser148@gmail.com>
2024-06-25 10:37:47 -0400

    Add content to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
+import Signup from "./components/Signup.jsx"
+import Login from "./components/Login.jsx"
+import LoginSignupHub from "./components/LoginSignupHub.jsx"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Settings from "./components/Settings.jsx"
+import Contact from "./components/Contact.jsx"
+import Profile from "./components/Profile.jsx"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-red-500 text-3xl">Testing this out.</h1>
-    </>
+    <main>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<LoginSignupHub />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} /> 
+          <Route path="settings" element={<Settings />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
   )
 }
 
