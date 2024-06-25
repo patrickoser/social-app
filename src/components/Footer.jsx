2a57fb4a8d787441721925ab2f2ab5861584ae39
Patrick Oser <patrick.oser148@gmail.com>
2024-06-25 10:20:54 -0400

    Add content to Footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="text-center sticky bottom-0">
+            <a href="https://github.com/patrickoser">My Github</a>
+        </footer>
+    )
+}
+
+export default Footer
