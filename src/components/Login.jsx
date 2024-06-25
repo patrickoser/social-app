ebc90a1a39763fc548c23c34eeedc893a4e18704
Patrick Oser <patrick.oser148@gmail.com>
2024-06-25 13:09:07 -0400

    Add padding to input boxes in Login.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
                                 autoComplete="email"
                                 onChange={(e) => setEmail(e.target.value)}
                                 required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -47,7 +47,7 @@ const Login = () => {
                                 autoComplete="current-password"
                                 onChange={(e) => setPassword(e.target.value)}
                                 required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
