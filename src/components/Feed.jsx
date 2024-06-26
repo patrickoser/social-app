96e9be3313c70da04da7a5d01e4c7f70f26d0be9
Patrick Oser <patrick.oser148@gmail.com>
2024-06-26 14:29:51 -0400

    Add some content to Feed.jsx.

---
+++ src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Feed = () => {
+    return (
+        //Maps each post to the Home page of the user.
+        <div>
+            <h1>One post</h1>
+            <h1>Two Post</h1>
+            <h1>Three Post</h1>
+        </div>
+    )
+}
