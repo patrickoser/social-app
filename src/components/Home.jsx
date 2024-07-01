7491eb3e7ba932a4a7581dca94b19c1a04f2fd85
Patrick Oser <patrick.oser148@gmail.com>
2024-07-01 13:04:38 -0400

    Add css to one sidebar in Home.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,26 @@ import Feed from "./Feed";
 const Home = () => {
     return (
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
-            <section></section>
-            <section>
+            <section className="flex-auto min-w-60 mt-5 px-5">
+
+            </section>
+            <section className="flex-auto">
                 <Feed />
             </section>
-            <section>
+            <section className="flex-auto">
             </section>
         </main>
     )
 }
 
 export default Home
+
+.sidebar {
+    border-right: 1px solid var(--twitter-background);
+    flex: 0.2;
+  
+    min-width: 250px;
+    margin-top: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
