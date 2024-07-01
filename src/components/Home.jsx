04955a8d8bb1c1f073c70f7fc64c5c6bd265921b
Patrick Oser <patrick.oser148@gmail.com>
2024-07-01 13:10:19 -0400

    Add comments to Home.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Feed from "./Feed";
 
 const Home = () => {
+    // Need to space the Home sections out more to give more room for Feed to display
+    // posts. Should probably be about twice as big as either individual sidebar.
     return (
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
             <section className="flex-auto min-w-60 mt-5 px-5 border">
