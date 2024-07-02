404526d5f45fbb81087f14fbdf322e3cd062e19d
Patrick Oser <patrick.oser148@gmail.com>
2024-07-02 15:42:58 -0400

    Set Home Feed container to proper width.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
             <section className="flex-auto min-w-60 mt-5 px-5 border">
             </section>
-            <section className="flex-auto text-center border">
+            <section className="flex-initial w-6/12 mt-5 px-5 text-center border">
                 <Feed />
             </section>
             <section className="flex-auto min-w-60 mt-5 px-5 border">
