e16b68ec5e618eb55bbf99fa50ac89d82c6b4c03
Patrick Oser <patrick.oser148@gmail.com>
2024-07-04 13:33:38 -0400

    Add content to Nav.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,2 +1,17 @@
 import React from "react";
 
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <ul  className="flex flex-row justify-between">
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/settings">Settings</Link></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
