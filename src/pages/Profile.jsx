989982b96b333d981278174f0f214d86e5f34d3d
Patrick Oser <patrick.oser148@gmail.com>
2024-07-04 13:40:20 -0400

    Adjust semantic elements iin Profile.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,17 +6,17 @@ const Profile = () => {
     return (
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
             
-            <section>
-                <form action="POST">
-                    <input 
-                        type="text"
-                        name="post"
-                        placeholder="What's on your mind?"
-                    />
-                    <button type="submit">Send</button>
-                </form>
-            </section>
-            <section>
+            <section id="profile-main-content">
+                <div>
+                    <form action="POST">
+                        <input 
+                            type="text"
+                            name="post"
+                            placeholder="What's on your mind?"
+                        />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
                 <div>
                     <Feed />
                 </div>
