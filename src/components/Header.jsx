80f2da27d8c5f37c609153b1ce57a0df63b31d04
Patrick Oser <patrick.oser148@gmail.com>
2024-07-04 13:25:53 -0400

    Small change.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
     return (
         <header className="flex justify-between">
             <section>
-                <Link to="/Home"><h2>Social App</h2></Link>
+                <Link to="/home"><h2>Social App</h2></Link>
             </section>
             <nav>
                 <ul className="flex flex-row justify-between">
-                    <li><Link to="/Profile">Profile</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/settings">Settings</Link></li>
                 </ul>
