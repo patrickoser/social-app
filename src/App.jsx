db05b8471f148bcd2f09ef382ff9c11296504c6e
Patrick Oser <patrick.oser148@gmail.com>
2024-07-04 11:08:42 -0400

    Correct imports in App to correct file path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
-import Signup from "./components/Signup.jsx"
-import Login from "./components/Login.jsx"
-import LoginSignupHub from "./components/LoginSignupHub.jsx"
+import Signup from "./pages/Signup.jsx"
+import Login from "./pages/Login.jsx"
+import LoginSignupHub from "./pages/LoginSignupHub.jsx"
 import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
-import Settings from "./components/Settings.jsx"
-import Contact from "./components/Contact.jsx"
-import Profile from "./components/Profile.jsx"
-import Home from "./components/Home.jsx"
+import Settings from "./pages/Settings.jsx"
+import Contact from "./pages/Contact.jsx"
+import Profile from "./pages/Profile.jsx"
+import Home from "./pages/Home.jsx"
 
 function App() {
 
