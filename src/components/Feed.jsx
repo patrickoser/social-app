deb5ef2ffb1fc080202fe9be41921da3210b2924
Patrick Oser <patrick.oser148@gmail.com>
2024-07-05 14:32:13 -0400

    Set border and color for posts.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,13 +4,13 @@ const Feed = () => {
     return (
         //Maps each post to the Home page of the user.
         <div>
-            <div className="">
+            <div className="max-w-full border border-red-500">
                 <h1>One post</h1>
             </div>
-            <div>
+            <div className="max-w-full border border-red-500">
                 <h1>Two Post</h1>
             </div>
-            <div>
+            <div className="max-w-full border border-red-500">
                 <h1>Three Post</h1>
             </div>  
             
