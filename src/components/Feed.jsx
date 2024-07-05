fac1185bb31c51f8fb1451506295038c71fee97d
Patrick Oser <patrick.oser148@gmail.com>
2024-07-05 14:20:09 -0400

    Wrap posts in containers.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,9 +4,18 @@ const Feed = () => {
     return (
         //Maps each post to the Home page of the user.
         <div>
-            <h1>One post</h1>
-            <h1>Two Post</h1>
-            <h1>Three Post</h1>
+            <div className="">
+                <h1>One post</h1>
+            </div>
+            <div>
+                <h1>Two Post</h1>
+            </div>
+            <div>
+                <h1>Three Post</h1>
+            </div>  
+            
+            
+            
         </div>
     )
 }
