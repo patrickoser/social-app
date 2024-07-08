5b5ac1b1a306bf14381f90e86cb1c6739c78a9ec
Patrick Oser <patrick.oser148@gmail.com>
2024-07-08 12:02:27 -0400

    Add like, share and save buttons to posts.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,16 +8,31 @@ const Feed = () => {
                 <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>This the content of the post. Where stuff will be written</p>
+                <div>
+                    <button>Like</button>
+                    <button>Share</button>
+                    <button>Save</button>
+                </div>
             </div>
             <div className="max-w-full border border-red-500 text-left">
                 <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>This is some more content to use as an example and figure out spacing</p>
+                <div>
+                    <button>Like</button>
+                    <button>Share</button>
+                    <button>Save</button>
+                </div>
             </div>
             <div className="max-w-full border border-red-500 text-left">
                 <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>Once again I will be writing more content because I am.</p>
+                <div>
+                    <button>Like</button>
+                    <button>Share</button>
+                    <button>Save</button>
+                </div>
             </div>  
         </div>
     )
