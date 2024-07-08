4f03a523e50a8a95d0e820f692c8896fa755310b
Patrick Oser <patrick.oser148@gmail.com>
2024-07-08 12:27:45 -0400

    Add TW to input in CreatePost.

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const CreatePost = () => {
     return (
-        <div>
+        <div className="">
             <form action="POST">
                 <input 
                     type="text"
                     name="post"
                     placeholder="What's on your mind?"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 />
                 <button type="submit">Send</button>
             </form>
