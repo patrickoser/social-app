992965c6adfcfcbfd70b5dbb68bc386fecdc20c0
Patrick Oser <patrick.oser148@gmail.com>
2024-07-08 11:59:00 -0400

    Add TW to post h1s.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,17 +5,17 @@ const Feed = () => {
         //Maps each post to the Home page of the user.
         <div>
             <div className="max-w-full border border-red-500 text-left">
-                <h1>Username</h1>
+                <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>This the content of the post. Where stuff will be written</p>
             </div>
             <div className="max-w-full border border-red-500 text-left">
-                <h1>Username</h1>
+                <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>This is some more content to use as an example and figure out spacing</p>
             </div>
             <div className="max-w-full border border-red-500 text-left">
-                <h1 className="">Username</h1>
+                <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>Once again I will be writing more content because I am.</p>
             </div>  
