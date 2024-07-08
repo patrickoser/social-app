dab7aa66ccaaec02a8c4450e48be42d30ac677b9
Patrick Oser <patrick.oser148@gmail.com>
2024-07-08 12:28:43 -0400

    Add padding to container in CreatePost.

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CreatePost = () => {
     return (
-        <div className="">
+        <div className="p-2">
             <form action="POST">
                 <input 
                     type="text"
