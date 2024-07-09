f4d935f47984adf9039e9ea9c917f3e61080b4f8
Patrick Oser <patrick.oser148@gmail.com>
2024-07-09 09:38:48 -0400

    Add TW to button in CreatePost.

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -8,9 +8,9 @@ const CreatePost = () => {
                     type="text"
                     name="post"
                     placeholder="What's on your mind?"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                 />
-                <button type="submit" className="ml-2">Send</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ml-2">Send</button>
             </form>
         </div>
     )
