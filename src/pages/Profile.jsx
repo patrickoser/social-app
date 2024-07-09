cff568b29dc647e51932355d981c6372283da4e8
Patrick Oser <patrick.oser148@gmail.com>
2024-07-09 09:52:11 -0400

    Add TW to profile-main-content.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Feed from "../components/Feed";
+import Nav from "../components/Nav";
 import CreatePost from "../components/CreatePost";
 
 const Profile = () => {
 
     return (
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
-            <section id="left-sidebar" className="flex-auto min-w-60 mt-5 px-5 border">
-            </section>
-            <section id="profile-main-content">
+            <div id="left-sidebar" className="flex-auto min-w-60 mt-5 px-5 border">
+                <Nav />
+            </div>
+            <div id="profile-main-content" className="flex-initial w-6/12 mt-5 px-5 text-center border">
                 <CreatePost />
                 <div>
                     <Feed />
                 </div>
-            </section>
-            <section id="right-sidebar" className="flex-auto min-w-60 mt-5 px-5 border"></section>
+            </div>
+            <div id="right-sidebar" className="flex-auto min-w-60 mt-5 px-5 border"></div>
         </main>
     )
 }
