d9c86e0f22aa8e653cd480e81445128cb75f22d1
Patrick Oser <patrick.oser148@gmail.com>
2024-07-09 09:33:37 -0400

    Add margin to CreatePost button.

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const CreatePost = () => {
     return (
         <div className="p-2">
-            <form action="POST">
+            <form action="POST" className="flex">
                 <input 
                     type="text"
                     name="post"
                     placeholder="What's on your mind?"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 />
-                <button type="submit">Send</button>
+                <button type="submit" className="ml-2">Send</button>
             </form>
         </div>
     )
