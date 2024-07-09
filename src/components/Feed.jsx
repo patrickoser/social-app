d66c6f9661db171384b55e6693956547e2f2b4fe
Patrick Oser <patrick.oser148@gmail.com>
2024-07-09 13:49:45 -0400

    Change color of post borders.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,7 +4,7 @@ const Feed = () => {
     return (
         //Maps each post to the Home page of the user.
         <div>
-            <div className="max-w-full border-b border-red-500 text-left">
+            <div className="max-w-full border-b border-black text-left">
                 <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>This the content of the post. Where stuff will be written</p>
@@ -14,7 +14,8 @@ const Feed = () => {
                     <button className="pr-1">Save</button>
                 </div>
             </div>
-            <div className="max-w-full border-b border-red-500 text-left">
+            <div className="max-w-full border-b border-black
+             text-left">
                 <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>This is some more content to use as an example and figure out spacing</p>
@@ -24,7 +25,7 @@ const Feed = () => {
                     <button className="pr-1">Save</button>
                 </div>
             </div>
-            <div className="max-w-full border-b border-red-500 text-left">
+            <div className="max-w-full border-b border-black text-left">
                 <h1 className="font-bold">Username</h1>
                 <h3 className="text-xs">Date of post</h3>
                 <p>Once again I will be writing more content because I am.</p>
