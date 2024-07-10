5ae935a6d3525cb9bfad344a2f904bd36e8c0b00
Patrick Oser <patrick.oser148@gmail.com>
2024-07-10 12:50:49 -0400

    Add bp to DataContext.jsx.

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -2,3 +2,17 @@ import { createContext, useEffect, useState } from "react";
 import api from '../api/postsAxios'
 import { useNavigate } from "react-router-dom";
 import { format } from 'date-fns'
+
+const DataContext = createContext({})
+
+export const DataProvider = ({ children }) => {
+    return (
+        <DataContext.Provider value={{
+            posts, setPosts
+        }}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export default DataContext
