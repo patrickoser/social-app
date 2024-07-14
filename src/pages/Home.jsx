3e6ae13e9c2f8b210352f1e0836830e32a607751
Patrick Oser <patrick.oser148@gmail.com>
2024-07-14 07:32:49 -0400

    Small change.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import CreatePost from "../components/CreatePost";
 const Home = () => {
     // Need to space the Home sections out more to give more room for Feed to display
     // posts. Should probably be about twice as big as either individual sidebar.
+    
     return (
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
             <section id="left-sidebar" className="flex-auto min-w-60 mt-5 px-5 border">
