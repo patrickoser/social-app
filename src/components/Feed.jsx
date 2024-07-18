dd251c000790d3a31255e794294def481e354069
Patrick Oser <patrick.oser148@gmail.com>
2024-07-18 13:49:40 -0400

    Refactor Feed to dynamically display posts.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,40 +1,13 @@
 import React from "react";
+import Post from "./Post";
 
-const Feed = () => {
+const Feed = ({ posts }) => {
     return (
         //Maps each post to the Home page of the user.
         <div>
-            <div className="max-w-full border-b border-black text-left">
-                <h1 className="font-bold">Username</h1>
-                <h3 className="text-xs">Date of post</h3>
-                <p>This the content of the post. Where stuff will be written</p>
-                <div className="flex justify-end">
-                    <button className="pr-1">Like</button>
-                    <button className="pr-1">Share</button>
-                    <button className="pr-1">Save</button>
-                </div>
-            </div>
-            <div className="max-w-full border-b border-black
-             text-left">
-                <h1 className="font-bold">Username</h1>
-                <h3 className="text-xs">Date of post</h3>
-                <p>This is some more content to use as an example and figure out spacing</p>
-                <div className="flex justify-end">
-                    <button className="pr-1">Like</button>
-                    <button className="pr-1">Share</button>
-                    <button className="pr-1">Save</button>
-                </div>
-            </div>
-            <div className="max-w-full border-b border-black text-left">
-                <h1 className="font-bold">Username</h1>
-                <h3 className="text-xs">Date of post</h3>
-                <p>Once again I will be writing more content because I am.</p>
-                <div className="flex justify-end">
-                    <button className="pr-1">Like</button>
-                    <button className="pr-1">Share</button>
-                    <button className="pr-1">Save</button>
-                </div>
-            </div>  
+            {posts.map(post => (
+                <Post key={post.id} post={post} />
+            ))}
         </div>
     )
 }
