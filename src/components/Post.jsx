49e0c0a5ff45ca2a5e539e1320eebac9a5a13553
Patrick Oser <patrick.oser148@gmail.com>
2024-07-18 13:40:28 -0400

    Add TW to Post elements and add in buttons.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,16 +3,21 @@ import { Link } from 'react-router-dom'
 
 const Post = () => {
     return (
-        <div>
+        <div className="max-w-full border-b border-black text-left">
             <Link to={`/post/${post.id}`}>
-                <h2>Username</h2>
-                <h5>{post.datetime}</h5>
+                <h2 className="font-bold">Username</h2>
+                <h5 className="text-xs">{post.datetime}</h5>
             </Link>
             <p>{
                 (post.content).length <= 100
                 ? post.content
                 : `${(post.content).slice(0, 100)}...`
             }</p>
+            <div className="flex justify-end">
+                <button className="pr-1">Like</button>
+                <button className="pr-1">Share</button>
+                <button className="pr-1">Save</button>
+            </div>
         </div>
     )
 }
