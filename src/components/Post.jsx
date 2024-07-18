5293249813b50c1c254ff8ab05f1c978174bcac1
Patrick Oser <patrick.oser148@gmail.com>
2024-07-18 13:36:05 -0400

    Add content to Post that gives structure to posts in Home.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 
+
+const Post = () => {
+    return (
+        <div>
+            <Link to={`/post/${post.id}`}>
+                <h2>Username</h2>
+                <h5>{post.datetime}</h5>
+            </Link>
+            <p>{
+                (post.content).length <= 100
+                ? post.content
+                : `${(post.content).slice(0, 100)}...`
+            }</p>
+        </div>
+    )
+}
