ed7a341ceebf7275d7cdf3e5ba0dd5db707df320
Patrick Oser <patrick.oser148@gmail.com>
2024-07-18 13:11:18 -0400

    Declare createPosts function in DataContext.

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -9,10 +9,27 @@ export const DataProvider = ({ children }) => {
 
     const [posts, setPosts] = useState([])
     const navigate = useNavigate()
+    const [postContent, setPostContent] = useState('')
+
+    const createPost = async (e) => {
+        e.preventDefault()
+        const id = posts.length ? posts[posts.length - 1].id + 1 : 1
+        const datetime = format(new Date(), 'MMMM dd yyyy pp')
+        const newPost = { id, content: postContent, datetime }
+        try {
+            const response = await api.post('/posts', newPost)
+            const allPosts = [...posts, response.data]
+            setPosts(allPosts)
+            setPostContent('')
+            navigate('/')
+        } catch(err) {
+            console.log(`Error: ${err}`)
+        }
+    }
 
     return (
         <DataContext.Provider value={{
-            posts, setPosts, navigate
+            posts, setPosts, navigate, postContent, setPostContent
         }}>
             {children}
         </DataContext.Provider>
