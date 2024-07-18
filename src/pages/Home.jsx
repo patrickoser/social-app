3937dd18a345239adffc4ff0eb77b1ec092acf99
Patrick Oser <patrick.oser148@gmail.com>
2024-07-18 13:56:32 -0400

    Pull in posts context with useContext.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Feed from "../components/Feed";
 import Nav from "../components/Nav";
 import PostForm from "../components/PostForm";
+import DataContext from "../context/DataContext";
 
 const Home = () => {
+
+    const { posts } = useContext(DataContext)
 
     return (
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
