32a5ea1488164df19559a103a5a586bec8ba9a96
Patrick Oser <patrick.oser148@gmail.com>
2024-07-19 15:06:51 -0400

    Update input attributes in PostForm.

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -9,8 +9,11 @@ const PostForm = () => {
             <form action="POST" className="flex">
                 <input 
                     type="text"
-                    name="post"
+                    value={postContent}
+                    name="postContent"
                     placeholder="What's on your mind?"
+                    onChange={(e) => setPostContent(e.target.value)}
+                    required
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                 />
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ml-2">Send</button>
