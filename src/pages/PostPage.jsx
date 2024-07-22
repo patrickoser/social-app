cd242c593ec8e74eadc4b9539d04c8178b88c930
Patrick Oser <patrick.oser148@gmail.com>
2024-07-22 12:25:45 -0400

    Import hooks/elements into PostPage.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useContext } from "react";
+import DataContext from "../context/DataContext";
+import { Link, useParams } from "react-router-dom"
 
 const PostPage = () => {
     return (
