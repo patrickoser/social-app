ee15948cbed1516fce94a3b11743d75271685ce1
Patrick Oser <patrick.oser148@gmail.com>
2024-07-22 12:46:20 -0400

    Add comments to PostPage.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -6,7 +6,10 @@ import { Link, useParams } from "react-router-dom"
 const PostPage = () => {
     const { posts } = useContext(DataContext)
     const { id } = useParams()
-    // 
+    // 'find()' loops through every post object to find the one that contains a 'post.id' that
+    //  strictly equals the 'id' that is being referenced by 'useParams'. When that check comes
+    // back as 'true' then the 'post' variable is assigned that object so that it can be referenced
+    // below to construct the page.
     const post = posts.find(post => (post.id).toString() === id)
 
     return (
