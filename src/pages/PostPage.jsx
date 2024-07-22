f0bf99563a032c9fedd1591065ad215e2cf47ca3
Patrick Oser <patrick.oser148@gmail.com>
2024-07-22 12:47:16 -0400

    Add 'post.postContent'/'deletePost' to PostPage.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -4,8 +4,12 @@ import DataContext from "../context/DataContext";
 import { Link, useParams } from "react-router-dom"
 
 const PostPage = () => {
+    // Pulls in the posts object from db.json for reference below.
     const { posts } = useContext(DataContext)
+
+    // useParams grabs the 'id' from the url for reference below.
     const { id } = useParams()
+
     // 'find()' loops through every post object to find the one that contains a 'post.id' that
     //  strictly equals the 'id' that is being referenced by 'useParams'. When that check comes
     // back as 'true' then the 'post' variable is assigned that object so that it can be referenced
@@ -17,6 +21,12 @@ const PostPage = () => {
             <div>
                 <h1>Username</h1>
                 <h2>{post.datetime}</h2>
+            </div>
+            <div>
+                <p>{post.postContent}</p>
+            </div>
+            <div>
+                <button onClick={() => deletePost(post.id)}>Delete</button>
             </div>
         </main>
     )
