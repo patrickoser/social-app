9ce07585412b2a1ae92abc8851589bb68eb65097
Patrick Oser <patrick.oser148@gmail.com>
2024-08-04 15:26:23 -0400

    Import components and methods into 'auth.jsx'.

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,6 +1,10 @@
+import { auth } from "../config/firebase"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+
 export const Auth = () => {
 
     return (
-        <div></div>
+        <div>
+        </div>
     )
 }
