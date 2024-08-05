c74f2e357a3a5f98fc30108ae3bac2a700c5ce84
Patrick Oser <patrick.oser148@gmail.com>
2024-08-05 14:26:38 -0400

    Remove signIn from DataContext.

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -12,16 +12,6 @@ export const DataProvider = ({ children }) => {
     const [posts, setPosts] = useState([])
     const navigate = useNavigate()
     const [postContent, setPostContent] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    const signIn = async () => {
-        try {
-            await createUserWithEmailAndPassword(auth, email, password)
-        } catch (err) {
-            console.error(err)
-        }
-    }
 
     const createPost = async (e) => {
         e.preventDefault()
@@ -65,7 +55,7 @@ export const DataProvider = ({ children }) => {
     return (
         <DataContext.Provider value={{
             posts, setPosts, navigate, postContent, setPostContent, createPost,
-            deletePost, signIn
+            deletePost
         }}>
             {children}
         </DataContext.Provider>
