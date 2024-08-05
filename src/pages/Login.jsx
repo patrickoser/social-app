ce0868d657276ecb179f7afda3373bdc3e1c8e95
Patrick Oser <patrick.oser148@gmail.com>
2024-08-05 14:34:57 -0400

    Add needed imports to Login.jsx.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom'
+import { auth } from '../config/firebase'
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
     const [email, setEmail] = useState('')
