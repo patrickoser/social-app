930ecfb6b972db6cc4668e745965c30e4d2b9be1
Patrick Oser <patrick.oser148@gmail.com>
2024-08-06 18:03:00 -0400

    Add import to LoginSignupHub.

--- src/pages/LoginSignupHub.jsx
+++ src/pages/LoginSignupHub.jsx
@@ -1,10 +1,11 @@
 import Signup from './Signup.jsx'
+import auth from '../config/firebase.jsx'
 
 const LoginSignupHub = () => {
     // Add a feature that checks local storage to see if they
     // already have an account before redirecting to Signup.
     console.log(auth?.currentUser?.email)
-    
+
     return (
         <main>
             <Signup />
