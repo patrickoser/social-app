874783b293f577bf03ddd59945eec531fec05dbb
Patrick Oser <patrick.oser148@gmail.com>
2024-08-08 11:52:55 -0400

    Add content to logout function in Settings.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
+import { signOut } from "firebase/auth";
 
 const Settings = () => {
+
+    const logout = async () => {
+        try {
+            await signOut(auth)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
         <div>
             <h1 className="text-center">Settings</h1>
             <Link to="/"><h1 className="text-center">LoginSignupHub</h1></Link>
-            <button></button>
+            <button onClick={logout}>Logout</button>
         </div>
     )
 }
