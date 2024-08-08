d871fbda7fd4afac21254dfaf9b28df325a8a826
Patrick Oser <patrick.oser148@gmail.com>
2024-08-08 11:48:15 -0400

    Import auth into Settings.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
 
 const Settings = () => {
     return (
         <div>
             <h1 className="text-center">Settings</h1>
             <Link to="/"><h1 className="text-center">LoginSignupHub</h1></Link>
+            <button></button>
         </div>
     )
 }
