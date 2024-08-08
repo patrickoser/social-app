1d1bf708ca909df74a375bb7a8e44a0128eb2c66
Patrick Oser <patrick.oser148@gmail.com>
2024-08-08 13:44:21 -0400

    Add navigate to logout.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
 import { signOut } from "firebase/auth";
+import DataContext from "../context/DataContext";
+import { useContext } from "react";
 
 const Settings = () => {
+
+    const { navigate } = useContext(DataContext)
 
     const logout = async () => {
         console.log(`User Status: ${auth?.currentUser?.email} has signed out.`)
         try {
             await signOut(auth)
+            navigate('/')
         } catch (err) {
             console.error(err)
         }
