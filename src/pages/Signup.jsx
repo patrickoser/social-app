bae07f2a534616c81e9900e9258754810961f371
Patrick Oser <patrick.oser148@gmail.com>
2024-08-10 16:45:56 -0400

    Adjust top margin of 'Already have an account' in Signup.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -79,7 +79,7 @@ const Signup = () => {
                         <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create Account</button>
                     </div>
                 </form>
-                <p className="mt-10 text-center text-sm text-gray-500">
+                <p className="mt-5 text-center text-sm text-gray-500">
                     Already have an account?&nbsp;
                     <Link to="/login"><button className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login here!</button></Link>
                 </p>
