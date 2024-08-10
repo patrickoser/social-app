2aa14168ea273092f7f2212fcdffe5c287d7a1c9
Patrick Oser <patrick.oser148@gmail.com>
2024-08-10 17:18:26 -0400

    Export googleProvider from firebase.jsx.

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
@@ -13,6 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
