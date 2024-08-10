f58d347677a467f4898f8a99f0d394f6412e867a
Patrick Oser <patrick.oser148@gmail.com>
2024-08-10 17:29:55 -0400

    Create signInWithGoogle function in Signup.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -26,6 +26,14 @@ const Signup = () => {
         }
     }
 
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
         <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
