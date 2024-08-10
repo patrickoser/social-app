6ab09c9897beb6f835538f08eb0249fd9b46043e
Patrick Oser <patrick.oser148@gmail.com>
2024-08-10 17:15:58 -0400

    Import signInWithPopup in Signup.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from '../config/firebase'
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 const Signup = () => {
     const [email, setEmail] = useState('')
