c0e48b3de2e218fa8bd947d879f3eed76f60db37
Patrick Oser <patrick.oser148@gmail.com>
2024-08-13 12:03:44 -0400

    Add redirect link back to home in Missing.

--- src/pages/Missing.jsx
+++ src/pages/Missing.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Home from "./Home";
 
 const Missing = () => {
     return (
         <div>
             <h1>Something went wrong.</h1>
+            <h3>Return <Link to={<Home />}>home</Link></h3>
         </div>
     )
 }
