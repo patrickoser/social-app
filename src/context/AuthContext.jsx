7d35343bbc4e39bdbb9f503a4c56296f15b28d80
Patrick Oser <patrick.oser148@gmail.com>
2024-08-14 11:52:46 -0400

    Define variables in AuthProvider.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,6 +4,11 @@ import { createContext, useState, useEffect } from "react";
 const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
+    const auth = getAuth()
+    const [user, setUser] = useState()
+    const [loading, setLoading] = useState(true)
+
+
     return (
         <AuthContext.Provider value={{}}>
             {children}
