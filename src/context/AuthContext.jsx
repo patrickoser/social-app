4738e2ead2bba12f9e9b6a9e4aa8e78737197319
Patrick Oser <patrick.oser148@gmail.com>
2024-08-14 11:50:49 -0400

    Create structure for AuthProvider context.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,3 +3,10 @@ import { createContext, useState, useEffect } from "react";
 
 const AuthContext = createContext({})
 
+export const AuthProvider = ({ children }) => {
+    return (
+        <AuthContext.Provider value={{}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
