2fe902bf979ac8c7afc5e17c7584fa4ec99e538e
Patrick Oser <patrick.oser148@gmail.com>
2024-08-14 12:29:37 -0400

    Add content to Protected.

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,3 +1,11 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext"
 import { Navigate } from "react-router-dom";
+
+const Protected = ({children}) => {
+    const {user} = useContext(AuthContext)
+
+    !user ? <Navigate to="/signup" replace /> : children
+}
+
+export default Protected
