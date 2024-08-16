694a97571d13c603469774a1deef77876d47897f
Patrick Oser <patrick.oser148@gmail.com>
2024-08-16 07:56:25 -0400

    Delete uneeded import DataContext.

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import api from '../api/postsAxios'
 import { useNavigate } from "react-router-dom";
 import { format } from 'date-fns'
 
