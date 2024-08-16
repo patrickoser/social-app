c803bc2c0cb4f3e017eaa034845b0922d71a120c
Patrick Oser <patrick.oser148@gmail.com>
2024-08-16 10:53:14 -0400

    Delete LoginSignupHub.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
 
 import Signup from "./pages/Signup.jsx"
 import Login from "./pages/Login.jsx"
-import LoginSignupHub from "./pages/LoginSignupHub.jsx"
 import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
 import Settings from "./pages/Settings.jsx"
@@ -14,7 +13,7 @@ import Missing from "./pages/Missing.jsx"
 
 import { DataProvider } from "./context/DataContext.jsx"
 import { AuthProvider } from "./context/AuthContext.jsx"
-import Protected from "./components/Protected.jsx"
+import { Protected } from "./components/Protected.jsx"
 
 function App() {
 
@@ -25,7 +24,7 @@ function App() {
           <AuthProvider>
             <Header />
             <Routes>
-              <Route path='/' element={<LoginSignupHub />} />
+              <Route path='/' element={<Protected><Home /></Protected>} />
               <Route path="signup" element={<Signup />} />
               <Route path="login" element={<Login />} />
                 <Route path="home" element={<Protected><Home /></Protected>} />
--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -2,10 +2,8 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext"
 import { Navigate } from "react-router-dom";
 
-const Protected = ({children}) => {
+export const Protected = ({children}) => {
     const { user } = useContext(AuthContext)
 
     !user ? <Navigate to="/signup" replace /> : children
 }
-
-export default Protected
