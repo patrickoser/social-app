e7e6767eb3e26379572ec0d3817a9164b68f6c69
Patrick Oser <patrick.oser148@gmail.com>
2024-08-16 10:59:35 -0400

    Add comments to Protected.

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -5,5 +5,9 @@ import { Navigate } from "react-router-dom";
 export const Protected = ({children}) => {
     const { user } = useContext(AuthContext)
 
+    // 'Navigate' will direct the page anywhere specified by the
+    // 'to' prop. 'Replace' makes sure the history stack is
+    // altered in a way so that the user can't press the back
+    // button and return to the protected page.
     !user ? <Navigate to="/signup" replace /> : children
 }
