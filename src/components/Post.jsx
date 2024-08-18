0926bb4c25042bfdb09a06c73b1c42386d41496a
Patrick Oser <patrick.oser148@gmail.com>
2024-08-18 15:05:29 -0400

    Install firebase-react-hooks.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 const Post = ({ post }) => {
+    
+
     return (
         <div className="max-w-full border-b border-black text-left">
             <Link to={`/post/${post.id}`}>
