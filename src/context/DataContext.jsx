08723e5c220a897289dd95132b818619c251711e
Patrick Oser <patrick.oser148@gmail.com>
2024-08-18 15:07:32 -0400

    Add more imports to DataContext.

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,7 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { format } from 'date-fns'
-import { db } from "../config/firebase"
+import { db, auth } from "../config/firebase"
+import { useAuthState } from "react-firebase-hooks/auth"
 import { collection, getDocs, addDoc, deleteDoc, query, where, doc } from "firebase/firestore"
 
 const DataContext = createContext({})
