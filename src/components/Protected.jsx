f732b48bf8e54569c88643aef397137bca4fb071
Patrick Oser <patrick.oser148@gmail.com>
2024-08-19 16:11:21 -0400

    Remove console log.

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -5,7 +5,6 @@ import { Navigate, Outlet } from "react-router-dom";
 export const PrivateRoutes = () => {
     // User holds auth info on current user if there is one.
     const { user } = useContext(AuthContext)
-    console.log(user)
 
     // 'Navigate' will direct the page anywhere specified by the
     // 'to' prop. 'Replace' makes sure the history stack is
