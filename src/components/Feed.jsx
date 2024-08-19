53f2694bde4c60606f9f0a6205d35052d96176f7
Patrick Oser <patrick.oser148@gmail.com>
2024-08-19 16:09:25 -0400

    Call posts context in Feed.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DataContext } from "../context/DataContext"
 import Post from "./Post";
 
 const Feed = () => {
+    const { posts } = useContext(DataContext)
+    
     return (
         //Maps each post to the Home page of the user.
         <div>
