14dc098bd6d656f434c1b10157e2087e5b6fdd71
Patrick Oser <patrick.oser148@gmail.com>
2024-08-20 13:26:59 -0400

    Add current user box to Nav.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,18 @@ import { Link } from "react-router-dom";
 const Nav = () => {
     return (
         <div>
-            <nav>
+            <nav className="">
                 <ul className="flex flex-col">
                     <li><Link to="/profile">Profile</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/settings">Settings</Link></li>
                 </ul>
+                {/* Pin this current user info to the bottom of the left margin.
+                Should be in a fixed position like the one on twitter. */}
+                <div className="inset-x-0 bottom-0">
+                    <div id="pfp">Image</div>
+                    <div id="username">username</div>
+                </div>
             </nav>
         </div>
     )
