052702ef86283a8b601163cecc87c64c3fbbe8e1
Patrick Oser <patrick.oser148@gmail.com>
2024-08-21 09:17:23 -0400

    Add username input field to signup.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -45,6 +45,20 @@ const Signup = () => {
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form onSubmit={handleSignup} className="space-y-6">
                     <div>
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div>
+                            <input 
+                                type="username"
+                                placeholder="Enter username..."
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                autoComplete="username"
+                                required
+                                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
                         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                         <div>
                             <input 
