3a29e1fa28927fee35efcabc20350b0a45430281
Patrick Oser <patrick.oser148@gmail.com>
2024-09-13 12:28:55 -0400

    Add upload button.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -55,6 +55,7 @@ const Profile = () => {
                 <div id="profile-bio">
                     <progress value={progress} max="100" />
                     <input type="file" onChange={handleChange} />
+                    <button onClick={handleUpload}>Upload</button>
                     {url && <img src={url} alt="uploaded" />}
                     <div id="profile-username">{user.username}</div>
                     <div id="bio">
