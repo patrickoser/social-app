9bbbdf856fa053ca64820da8779f0e7cae0e5af1
Patrick Oser <patrick.oser148@gmail.com>
2024-09-15 12:53:31 -0400

    Import getStorage to firebase.jsx.

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 import { getAnalytics } from "firebase/analytics"
+import { getStorage } from "firebase/storage"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
