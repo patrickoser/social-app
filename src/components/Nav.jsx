77796bf3d1f90ca5d5af75fbf95dc998f9572a30
Patrick Oser <patrick.oser148@gmail.com>
2024-09-20 11:58:27 -0400

    Removed unused import.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { auth } from "../config/firebase";
 import { ref, getDownloadURL, listAll } from "firebase/storage";
 
 const Nav = () => {
