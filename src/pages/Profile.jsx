d0fdcba3da8ee212dceb57e092a2fe6b83c3d8c4
Patrick Oser <patrick.oser148@gmail.com>
2024-09-20 09:53:25 -0400

    Add console.log.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -26,6 +26,7 @@ const Profile = () => {
         The ref() function takes two arguments: the storage instance and the path where the file will be 
         stored. In this case, the image will be stored in the "images" folder and the name of the image 
         will be the original name of the file. */
+        console.log(user)
         const storageRef = ref(storage, `users/${user.uid}/${image.name}`)
 
         /* This line starts the upload of the image to Firebase Storage. The uploadBytesResumable() 
