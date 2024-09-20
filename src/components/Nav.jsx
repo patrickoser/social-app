f746d48590179976ff708fe7936cae69576920c5
Patrick Oser <patrick.oser148@gmail.com>
2024-09-20 12:05:35 -0400

    Import useEffect and useState.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
