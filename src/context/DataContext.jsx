7dbb6b0d2c69a5c6078b9a8a84ead05fd83425c6
Patrick Oser <patrick.oser148@gmail.com>
2024-10-30 09:17:39 -0400

    Import useContext in DataContext.

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { format } from 'date-fns'
 import { db } from "../config/firebase"
