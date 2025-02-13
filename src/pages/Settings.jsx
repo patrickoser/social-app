e287919e0d1953d1c89374cb411e4592641d77c5
Patrick Oser <patrick.oser148@gmail.com>
2025-02-13 13:55:13 -0500

    Add css to Settings main element.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -22,7 +22,7 @@ const Settings = () => {
     }
 
     return (
-        <main>
+        <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
             <div id="left-sidebar" className="flex-auto min-w-60 mt-5 px-5 border">
                 <Nav />
             </div>
