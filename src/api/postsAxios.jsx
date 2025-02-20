f769594fab469d41557bb37b18db866fa67c5a2b
Patrick Oser <patrick.oser148@gmail.com>
2025-02-20 09:19:33 -0500

    Delete axios files from project.

--- src/api/postsAxios.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-// Create a server at the baseURL.
-// Then in the CLI start the json-server to run using this url and watch 'data/db.json'
-// for changes.
-// CLI 'npx json-server -p 3500 -w data/db.json'
-export default axios.create({
-    baseURL: 'http://localhost:3500'
-});
