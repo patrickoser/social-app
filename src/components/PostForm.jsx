2bdacfdcb2af4e1d3b581eb73eb75be2e993cdab
Patrick Oser <patrick.oser148@gmail.com>
2025-02-22 10:01:05 -0500

    Update Button name.

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -21,7 +21,7 @@ const PostForm = () => {
                     onChange={(e) => setPostContent(e.target.value)}
                     required
                 />
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ml-2">Send</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ml-2">Post</button>
             </form>
         </div>
     )
