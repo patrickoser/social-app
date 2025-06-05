f80aaf3bbcd536615d2769932355d1fc904a98e6
Patrick Oser <patrick.oser148@gmail.com>
2025-06-05 15:50:11 -0400

    Remove comments.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,6 @@ import { AuthContext } from "../context/AuthContext";
 
 const Post = ({ post }) => {
     /* useAuthState gives information about current user */
-    /* const [user] = useAuthState(auth) */
     const { user } = useContext(AuthContext)
 
     /* Used to hold and set the state of the number of likes. */
