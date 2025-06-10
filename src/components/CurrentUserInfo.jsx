3180eca2320e7fe4f677a17161538b93eee53dbe
Patrick Oser <patrick.oser148@gmail.com>
2025-06-10 13:53:13 -0400

    Create new component for current user and import whats needed.

---
+++ src/components/CurrentUserInfo.jsx
@@ -0,0 +1,8 @@
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ref, getDownloadURL, listAll } from "firebase/storage";
+import { storage } from "../config/firebase";
+
+const CurrentUserInfo = () => {
+    
+}
