4c2061f3805bafce397066d6f4cc241df5c20d6b
Patrick Oser <patrick.oser148@gmail.com>
2025-06-10 16:18:56 -0400

    Update Home css.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,18 +11,18 @@ const Home = () => {
 
     return (
         <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
-            <section id="left-sidebar" className="flex-auto min-w-60 mt-5 px-5 border">
+            <div id="left-sidebar" className="w-3/12 min-w-60 mt-5 px-5 border">
                 <Nav />
-            </section>
-            <section id="home-main-content" className="flex-initial w-6/12 mt-5 px-5 text-center border">
+            </div>
+            <div id="home-main-content" className="flex-initial w-6/12 mt-5 px-5 text-center border">
                 <PostForm />
                 <div>
                     {postIsLoading ? <p>Loading...</p> : posts.length ? <Feed posts={posts} /> : <p>No posts to display</p>}
                 </div>
-            </section>
-            <section id="right-sidebar" className="flex-auto min-w-60 mt-5 px-5 border">
+            </div>
+            <div id="right-sidebar" className="w-3/12 min-w-60 mt-5 px-5 border">
                 <CurrentUserInfo />
-            </section>
+            </div>
         </main>
     )
 }
