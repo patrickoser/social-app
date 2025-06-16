03632feff31a037497a6fea0f3d9ef086c650002
Patrick Oser <patrick.oser148@gmail.com>
2025-06-16 15:48:48 -0400

    Add css to Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="text-center sticky bottom-0">
-            <a href="https://github.com/patrickoser">My Github</a>
+        <footer className="text-center sticky bottom-0 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 py-4">
+            <a href="https://github.com/patrickoser" className="hover:text-gray-900 dark:hover:text-white">My Github</a>
         </footer>
     )
 }
