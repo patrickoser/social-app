279286c3b932aafff49fb2c6cc9d1d32b75c82e8
Patrick Oser <patrick.oser148@gmail.com>
2025-06-16 13:30:36 -0400

    Small css change in PostForm.

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -21,7 +21,7 @@ const PostForm = () => {
                     onChange={(e) => setPostContent(e.target.value)}
                     required
                 />
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ml-2">Post</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ml-2">Post</button>
             </form>
         </div>
     )
