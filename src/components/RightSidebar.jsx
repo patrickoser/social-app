7fb08676528d51a27265f8c2cca7ef8037fa844d
Patrick Oser <patrick.oser148@gmail.com>
2025-06-23 19:09:17 -0400

    Add border to inside of sidebars.

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,7 +3,7 @@ import CurrentUserInfo from "./CurrentUserInfo";
 
 const RightSidebar = () => {
     return (
-        <section id="right-sidebar" className="w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800">
+        <section id="right-sidebar" className="w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800 border-l border-gray-200 dark:border-gray-700">
             <CurrentUserInfo />
         </section>
     )
