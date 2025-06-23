046440f26617091fccf7c6574d68f20f75986efc
Patrick Oser <patrick.oser148@gmail.com>
2025-06-23 18:45:53 -0400

    Create leftsidebar component.

---
+++ src/components/LeftSidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+const LeftSidebar = () => {
+    return (
+        <section id="left-sidebar" className="w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800">
+            <Nav />
+        </section>
+    )
+}
+
+export default LeftSidebar 
