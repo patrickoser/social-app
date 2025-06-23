d5e4787bddac373cd19a1f1503b941953f1058b2
Patrick Oser <patrick.oser148@gmail.com>
2025-06-23 17:20:02 -0400

    Adjust Post to receive necessary likes functions/state from DC.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,110 +1,14 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from 'react-router-dom'
-import { addDoc, getDocs, collection, query, where, deleteDoc, doc } from "firebase/firestore";
-import { useEffect, useState, useContext } from "react";
-import { db } from "../config/firebase"
 import { AuthContext } from "../context/AuthContext";
+import { DataContext } from "../context/DataContext";
 
 const Post = ({ post }) => {
-    /* useAuthState gives information about current user */
     const { user } = useContext(AuthContext)
+    const { likes, getLikes, addLike, removeLike, hasUserLiked } = useContext(DataContext)
 
-    /* Used to hold and set the state of the number of likes. */
-    const [likes, setLikes] = useState(null)
-
-    /* Reference the likes collection in firestore */
-    const likesRef = collection(db, "likes")
-
-    /* Reference the 'likes' collection, using the 'likesRef'. 'where()' then loops
-    through searching for the postId, in the 'likes' collection that matches the
-    'post.id' of each individual post. */
-    const likesDoc = query(likesRef, where("postId", "==", post.id))
-
-    /* Updates the 'likes' state by referencing the likes collection docs using the
-    variables defined above. */
-    const getLikes = async () => {
-        try {
-            /* 'getDocs' references pulls data from 'likesDoc' and stores it in 'data' */
-            const data = await getDocs(likesDoc)
-
-            /*  */
-            setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })))
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    /* Creates a new doc within the 'likes' collection, taking the 'user.userId' and 
-    'post.id' and storing it for reference. */
-    const addLike = async () => {
-        try {
-            const newDoc = await addDoc(likesRef, {
-                userId: user?.userId,
-                username: user?.username,
-                postId: post.id
-            })
-            /* Checks the 'user' auth and sets the state of the likes. If there was 
-            already likes in the array those are set first with the new like data 
-            added to the end of the array. */
-            if (user) {
-                setLikes((prev) => 
-                    prev
-                        ?[...prev, {userId: user.userId, username: user.username, likeId: newDoc.id }]
-                        : [{ userId: user.userId, username: user.username, likeId: newDoc.id }]
-                )
-            }
-        /* Catch block for potential errors. */ 
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    /* Deletes likes from the 'likes' collection array. */
-    const removeLike = async () => {
-        /* Query the 'likes' collection to find the doc that holds a 'postId'/'userId 
-        that matches the current users 'user.userId' and 'post.id' */ 
-        try {
-            const likeToDeleteQuery = query(
-                likesRef,
-                where("postId", "==", post.id),
-                where("userId", "==", user?.userId)
-            )
-
-            /* Call 'getDocs' on the query and assign data to 'likeToDeleteData'. */
-            const likeToDeleteData = await getDocs(likeToDeleteQuery)
-
-            /* Get the doc id associated with the like and assign to 'likeId'. */
-            const likeId = likeToDeleteData.docs[0].id
-
-            /* Use 'likeId' to refernce the specific like you want to delete in the 
-            'likes' collection. */
-            const likeToDelete = doc(db, "likes", likeId)
-
-            /* Pass the specified like to 'deleteDoc' function to remove it from 
-            the 'likes' firestore collection. */
-            await deleteDoc(likeToDelete)
-            /* Check for user auth, if true... */
-            if (user) {
-                /* Loop through 'likes' state array and remove the like that matches 
-                the 'likeId'. */
-                setLikes(
-                    (prev) => prev && prev.filter((like) => like.likeId !== likeId)
-                )
-            }
-        /* Catch block for potential errors. */
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    /* Loop through 'likes' state array and check if the current user.userId matches any 
-    user id associated with the post. If no match is found then that means the user 
-    hasn't liked that post. */
-    const hasUserLiked = likes?.find((like) => like.userId === user?.userId)
-
-    /* Calls 'getLikes' function on page load or change to dependent variables. */ 
     useEffect(() => {
-        getLikes()
+        getLikes(post.id)
     }, [post.id, user?.userId])
 
     return (
@@ -120,7 +24,7 @@ const Post = ({ post }) => {
             }</p>
             <div className="flex justify-end mt-2">
                 <button 
-                    onClick={hasUserLiked ? removeLike : addLike} 
+                    onClick={hasUserLiked(user) ? () => removeLike(post.id, user) : () => addLike(post.id, user)} 
                     className="pr-1 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
                 >Like</button>
                 {likes && <p className="text-gray-600 dark:text-gray-300"> {likes?.length} </p>}
