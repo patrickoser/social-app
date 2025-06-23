991dfd91093b55df74bd53b0b85c15bd916bbba6
Patrick Oser <patrick.oser148@gmail.com>
2025-06-23 18:54:06 -0400

    Add new components to PostPage.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -2,11 +2,10 @@ import React, { useContext, useEffect } from "react";
 import { DataContext } from "../context/DataContext";
 import { AuthContext } from "../context/AuthContext";
 import { useParams } from "react-router-dom"
-import Nav from "../components/Nav";
-import CurrentUserInfo from "../components/CurrentUserInfo";
+import LeftSidebar from "../components/LeftSidebar";
+import RightSidebar from "../components/RightSidebar";
 
 const PostPage = () => {
-
     const { posts, deletePost, postIsLoading, likes, getLikes, addLike, removeLike, hasUserLiked } = useContext(DataContext)
     const { user } = useContext(AuthContext)
     const { id } = useParams()
@@ -22,10 +21,8 @@ const PostPage = () => {
                 <h1 className="text-center text-gray-900 dark:text-white">Loading...</h1>
             ) : (    
                 <main className="flex h-screen max-w-7xl mx-auto py-0 px-3">
-                    <section id="left-sidebar" className="w-3/12 min-w-60 mt-5 px-5 border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800">
-                        <Nav />
-                    </section>
-                    <section id="main-content-post" className="flex-initial w-6/12 mt-5 px-5 text-center border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-white">
+                    <LeftSidebar />
+                    <section id="main-content-post" className="flex-initial w-6/12 mt-5 px-5 text-center bg-white dark:bg-gray-800 text-gray-900 dark:text-white">
                         <div className="max-w-full text-left p-4">
                             <h1 className="text-xl font-bold text-gray-900 dark:text-white">{post?.username}</h1>
                             <h2 className="text-xs text-gray-500 dark:text-gray-400">{post?.datetime}</h2>
@@ -45,9 +42,7 @@ const PostPage = () => {
                             >Delete</button>
                         </div>
                     </section>
-                    <section id="right-sidebar" className="w-3/12 min-w-60 mt-5 px-5 border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800">
-                        <CurrentUserInfo />
-                    </section>
+                    <RightSidebar />
                 </main>
             )}
         </div>
