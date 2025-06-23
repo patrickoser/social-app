e6a895eef1c6effaffecf46fb650b9e9a0dcfc50
Patrick Oser <patrick.oser148@gmail.com>
2025-06-23 18:46:48 -0400

    Create right sidebar component.

---
+++ src/components/RightSidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CurrentUserInfo from "./CurrentUserInfo";
+
+const RightSidebar = () => {
+    return (
+        <section id="right-sidebar" className="w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800">
+            <CurrentUserInfo />
+        </section>
+    )
+}
+
+export default RightSidebar 
