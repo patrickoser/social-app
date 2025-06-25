fe14a14f383f86576d552119c41258f126239af4
Patrick Oser <patrick.oser148@gmail.com>
2025-06-25 15:31:38 -0400

    Adjust bottom border of header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const { isDarkMode, toggleTheme } = useContext(ThemeContext);
     
     return (
-        <header className="bg-white dark:bg-gray-800 shadow-sm pt-2">
+        <header className="bg-white dark:bg-gray-800 shadow-sm pt-2 border-b border-gray-200 dark:border-gray-700">
             <div className="flex max-w-7xl mx-auto py-0 px-3">
                 <div className="w-3/12 min-w-60 px-5">
                     <Link to="/home">
