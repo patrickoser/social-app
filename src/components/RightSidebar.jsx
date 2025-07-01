416625bd54096776df80ccc69b1c3389ddbba0cb
Patrick Oser <patrick.oser148@gmail.com>
2025-07-01 14:35:14 -0400

    Adjust css in RightSidebar.

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,10 +3,10 @@ import CurrentUserInfo from "./CurrentUserInfo";
 
 const RightSidebar = () => {
     return (
-        <section id="right-sidebar" className="w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800">
+        <section id="right-sidebar" className="hidden md:block w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800">
             <CurrentUserInfo />
         </section>
     )
 }
 
-export default RightSidebar 
+export default RightSidebar
