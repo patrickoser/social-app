b57ac96b03edd021e9e6cb19689e65b2051da913
Patrick Oser <patrick.oser148@gmail.com>
2025-07-01 15:29:38 -0400

    Adjust padding in PostPage.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -20,7 +20,7 @@ const PostPage = () => {
             ) : (
                 <main className="flex flex-col md:flex-row max-w-7xl mx-auto py-0 px-3">
                     <LeftSidebar />
-                    <section className="flex-1 w-full md:w-6/12 mt-5 px-3 md:px-5 text-center bg-white dark:bg-gray-800 text-gray-900 dark:text-white border-t border-b border-gray-300 dark:border-gray-700 pb-20 md:pb-0">
+                    <section className="flex-1 w-full md:w-6/12 mt-5 px-3 md:px-5 text-center bg-white dark:bg-gray-800 text-gray-900 dark:text-white border-t border-b border-gray-300 dark:border-gray-700 pb-7 md:pb-0">
                         <div className="max-w-full text-left p-4">
                             <div className="flex items-center space-x-3">
                                 <ProfilePicture userId={post?.userId} size="lg" />
