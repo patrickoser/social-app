f569ab9a5972693394fc5d4f1bfe67695ae8fd11
Patrick Oser <patrick.oser148@gmail.com>
2025-07-01 15:36:19 -0400

    Add Nav back to leftsidebar.

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Nav from "./Nav";
 
 const LeftSidebar = () => {
     return (
         <section id="left-sidebar" className="hidden md:block w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800">
-            {/* Nav component removed - now rendered directly in pages */}
+            <Nav />
         </section>
     )
 }
