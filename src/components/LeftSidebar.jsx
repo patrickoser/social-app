7a91518c9fcc0d6df89f0023fccaf9365f1b08c2
Patrick Oser <patrick.oser148@gmail.com>
2025-08-11 10:35:59 -0400

    Comments.

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./Nav";
 
+/* Only visible above 768px(md breakpoint). */
 const LeftSidebar = () => {
     return (
         <section id="left-sidebar" className="hidden md:block w-3/12 min-w-60 mt-5 px-5 bg-white dark:bg-gray-800">
