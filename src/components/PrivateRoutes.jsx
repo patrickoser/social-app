0a72fa5e97ca8d5d85d33fe65eae2201bfb31a78
Patrick Oser <patrick.oser148@gmail.com>
2025-08-12 15:51:35 -0400

    Comments.

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -5,10 +5,9 @@ import { Navigate, Outlet } from "react-router-dom";
 export const PrivateRoutes = () => {
     const { user } = useContext(AuthContext)
 
-    /* 'Navigate' will direct the page anywhere specified by the
-    'to' prop. 'Replace' makes sure the history stack is
-    altered in a way so that the user can't press the back
-    button and return to the protected page. */
+    /* Auth check. If user is not logged in, redirect to signup page. If they
+    they are logged in, show the protected page they are requesting.
+    Replace is used to prevent the user from going back to the protected page. */
     return (
         !user ? <Navigate to="/signup" replace /> : <Outlet />
     )
