9ede789c067312105d72dd473db6c000a381b97f
Patrick Oser <patrick.oser148@gmail.com>
2025-08-17 11:34:21 -0400

    Remove unused code.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import PostForm from "../components/PostForm";
 import LeftSidebar from "../components/LeftSidebar";
 import RightSidebar from "../components/RightSidebar";
 import MobileNav from "../components/MobileNav";
-// Guest mode comment: Guest indicator component for showing guest status
 import GuestIndicator from "../components/GuestIndicator";
 import { DataContext } from "../context/DataContext";
 
@@ -16,7 +15,6 @@ const Home = () => {
             <main className="flex flex-col md:flex-row max-w-7xl mx-auto py-0 px-3">
                 <LeftSidebar />
                 <section className="flex-1 w-full md:w-6/12 mt-5 px-3 md:px-5 text-center bg-white dark:bg-gray-800 text-gray-900 dark:text-white border-t border-gray-300 dark:border-gray-700 pb-20 md:pb-0">
-                    {/* Guest mode comment: Show guest indicator when in guest mode */}
                     <GuestIndicator />
                     <PostForm />
                     <div>
