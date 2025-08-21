3bbf74a5651f32f2ebfe58180817e30f7c12c750
Patrick Oser <patrick.oser148@gmail.com>
2025-08-21 12:20:47 -0400

    Reorg code in a semantically appropriate structure.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,6 +21,15 @@ const Login = () => {
             navigate('/home')
         }
     }, [user, authLoading, navigate])
+    
+    /* Show loading if auth is still being determined */
+    if (authLoading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-800">
+                <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600 dark:border-indigo-400"></div>
+            </div>
+        )
+    }
 
     const handleSignIn = async (e) => {
         e.preventDefault()
@@ -44,15 +53,6 @@ const Login = () => {
         signInAsGuest();
         /* Navigation will be handled by useEffect when user state updates */
     };
-
-    /* Show loading if auth is still being determined */
-    if (authLoading) {
-        return (
-            <div className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-800">
-                <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600 dark:border-indigo-400"></div>
-            </div>
-        )
-    }
 
     return (
         <main className="flex flex-col justify-center px-6 py-12 lg:px-8 bg-white dark:bg-gray-800">
