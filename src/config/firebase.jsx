d98a65d968516e227104c8d55bf57bcfa415ee71
Patrick Oser <patrick.oser148@gmail.com>
2025-08-25 13:40:31 -0400

    Create env file and add to gitignore.

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARqGC0xCN_vrcs3Q33VkekkmhGZpKJqfE",
-  authDomain: "social-app-6ef03.firebaseapp.com",
-  projectId: "social-app-6ef03",
-  storageBucket: "social-app-6ef03.appspot.com",
-  messagingSenderId: "398967320699",
-  appId: "1:398967320699:web:bc212665c5f882a9b6b2de",
-  measurementId: "G-10PHQ3HCMC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig)
